e2ee9a658adab83ad3e3b148d33cedf07290161c
HansKalsi <hans.kalsi@yahoo.com>
2023-12-02 18:06:35 +0000

    initial commit

---
+++ Roman_Logic.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Roman_Logic : MonoBehaviour
+{
+    public GameObject skin; // Skin
+    public string unitName; // title
+    public string[] nickname; // commendation
+    public int[] number; // strength
+    private int fightingNumbers; // fighting capacity
+    private int averageMorale; // willingness to fight
+    private int averageHealth; // health of units
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
